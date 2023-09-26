1f564afe9f882331a268520af070f1f0b6e4db32
roshaans2 <roshaansathya@gmail.com>
2023-09-26 15:49:43 +0530

    Fetching data from client side using axios

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,7 +1,37 @@
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+
 export default function Books(){
+    const [books,setBooks] = useState([])
+
+    useEffect(()=>{
+       const fetchAllBooks = async() => {
+        try {
+            const res = await axios.get("http://localhost:8000/books")
+            setBooks(res.data);
+        } catch (error) {
+            console.log(error)
+        }
+       }
+       fetchAllBooks()
+    },[])
+
     return(
         <div>
-            Books Page
+            <h1>Book Shop</h1>
+            <div className="books">
+                {books.map((book)=>(
+                    <div className="book" key={book.id}>
+                    {book.cover && <img src={book.cover} alt="" />}
+                    <h2>{book.title}</h2>
+                    <p>{book.descp}</p>
+                    <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+            <button><Link to="/add">Add new book</Link></button>
         </div>
     )
 }
