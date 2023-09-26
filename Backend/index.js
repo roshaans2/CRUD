e0134db4ce499792e7eae007173e228d01883908
roshaans2 <roshaansathya@gmail.com>
2023-09-26 15:01:09 +0530

    Read data from Database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,28 @@
 const express = require("express")
+const mysql = require("mysql")
 
 const app = express();
 
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Datascience@2024",
+    database:"test"
+})
 
-app.listen(6000,()=>{
+app.get("/",(req,res)=>{
+    res.send("Hello this is Backend")
+})
+
+app.get("/books",(req,res)=>{
+    const q = "SELECT * FROM books";
+    db.query(q,(err,data)=>{
+        if(err) res.json(err)
+        return res.json(data)
+    })
+})
+
+
+app.listen(8000,()=>{
     console.log("Connected to Backend");
 })
